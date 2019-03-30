17972553cbccbb21ddd8f9e624ec704f1af660b7
Mark Romero <markromero@marks-mbp.lan>
2019-03-29 23:36:48 -0600

    completed style for auth page

--- src/Components/Auth/Auth.js
+++ src/Components/Auth/Auth.js
@@ -1,24 +1,26 @@
 import React, {Component} from 'react';
+import heloLogo from '../../assest/helo_logo.png';
+
+import './Auth.css';
 
 class Auth extends Component {
     render() {
         return (
             <section className='authSection'>
                 <div className='login-section'>
-                    <div className='login-img'> smile </div>
+                    <div className='login-img'> <img src={heloLogo} alt="logo"/> </div>
                     <div className='login-title'> Helo </div>
-                    <div className='login-form'>
-                        <div className='login-group'>
-                            <label>Username:</label>
-                            <input className='login-form-input' />
-                        </div>
-                        <div className='login-group'>
-                            <label>Password:</label>
-                            <input className='login-form-input' />
-                        </div>
+                    <div className='login-username-group'>
+                        <label>Username:</label>
+                        <input className='login-form-input' />
                     </div>
-                    <button>Login</button>
-                    <button>Register</button>
+                    <div className='login-password-group'>
+                        <label>Password:</label>
+                        <input className='login-form-input' />
+                    </div>
+                    <button className='btn btn-form btn-login'>Login</button>
+                    <button className='btn btn-form btn-register'>Register</button>
+
                 </div>
             </section>
         )
