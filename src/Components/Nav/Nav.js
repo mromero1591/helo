ad5f8385e3aaec8af10d92a4e6df884ec651fd00
Mark Romero <markromero@Marks-MacBook-Pro.local>
2019-03-30 10:32:22 -0600

    completed inital styles for navbar

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -3,14 +3,17 @@ import {Link} from 'react-router-dom';
 
 //Custom imports
 import './Nav.css';
+import homeLogo from '../../assest/home_logo.png';
+import newLogo from '../../assest/new_logo.png';
+import shutDownLogo from '../../assest/shut_down.png';
 
 class Nav extends Component {
   render() {
     return (
       <nav className='navbar'>
-        <Link to='/dashboard'><button>Home</button></Link>
-        <Link to='/form'><button>New Post</button></Link>
-        <Link to='/'><button>Logout</button></Link>
+        <Link className='btn-link btn-home' to='/dashboard'><img src={homeLogo} /></Link>
+        <Link className='btn-link btn-newpost' to='/form'><img src={newLogo}/></Link>
+        <Link to='/' className='btn-link btn-logout'><img src={shutDownLogo}/></Link>
       </nav>
     )
   }
